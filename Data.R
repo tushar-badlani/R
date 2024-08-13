3e9ceacc65a1c9a8947e1d9f15930c0b9b7b88dd
Tushar <tusharbadlani0@gmail.com>
2024-08-13 15:32:38 +0530

    Create dataset

---
+++ Data.R
@@ -0,0 +1,6 @@
+#creating own dataset
+Serial_no = c(1,2,3,4)
+City_name= c("Pune", "Mumbai", "Delhi", "Chennai")
+Temprature = c(34, 38,25, 39)
+City_dataset = data_frame(Serial_no, City_name, Temprature)
+view(City_dataset)
